5a1968255b18cff984fff8acbbf0057ba6e9b090
yasergirit <yasergirit@gmail.com>
2022-07-19 17:41:55 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,44 @@
+# configuration
+from settings import settings
+
+# discord
+import discord
+from requests import post
+
+# regex
+import re
+
+# base64
+import io
+from base64 import b64decode
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
+
+    if message.content.startswith('!generate:'):
+        await message.channel.send("Making the request - it may take a while")
+        req = post(url = settings.URL, json = {'prompt': message.content.split("!generate:")[1].strip()})
+        await message.channel.send("Result is completed")
+        
+        images = re.search('\[(.*)\]', req.content.decode("utf-8") ).group(0)
+        encodings = [e.replace("\\n", "") for e in re.findall('"([^"]*)"', images)]
+        myfiles = []
+        for i, encoded in enumerate(encodings):
+            with open("img/image{}.png".format(i), "wb") as fh:
+                fh.write(b64decode(encoded))
+            myfiles.append(discord.File("img/image{}.png".format(i)))
+        
+        await message.channel.send(files = myfiles)
+
+client.run(config.TOKEN)
